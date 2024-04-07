a7ff621abcf502ff061827561147b62f471de253
jo <jo@MacBook-Pro-de-jo.local>
2024-04-07 09:21:43 +0200

    Move movies.json to ./

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const crypto = require('node:crypto') // per crear un id únic
 const { validateMovie } = require('./schemas/movies')
-const movies = require('./web/movies.json')
+const movies = require('./movies.json')
 const app = express()
 
 // S e t t i n g s
